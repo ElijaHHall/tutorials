3262a10dbdcedd4cd930d1285408057b7ac827e8
Elijah Hall <ehall851@gmail.com>
2019-06-04 09:11:53 -0600

    alert appears when button is clicked

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function doSome() {
+  alert('you clicked me!')
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button>click me</button>
+        <button onClick={doSome}>click me</button>
       </header>
     </div>
   );
